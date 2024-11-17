d0fa7ec757795d0bc70b1d5db562f4713dffe3e8
khemrajpbits <167669513+khemrajpbits@users.noreply.github.com>
2024-11-17 22:47:02 +0530

    Update app.js

--- app.js
+++ app.js
@@ -51,7 +51,7 @@ app.use('/', menus);
 app.use('/', orders);
 app.use('/', restaurants);
 
-mongoose.connect(process.env.PORT.MONGODB_URI, { })
+mongoose.connect('mongodb+srv://Cluster95194:Lavleen@cluster95194.fiw1c.mongodb.net/food_delivery_db', { })
     .then(() => console.log('MongoDB connected'))
     .catch(err => console.log(err));
 
